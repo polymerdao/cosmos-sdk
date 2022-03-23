bb169e00a77870b92ba859335d4a85776e1a233e
Sai Deng <dengsai@gmail.com>
2022-03-22 19:41:28 -0700

    Add multistore proof tests

--- store/v2/multi/proof.go
+++ store/v2/multi/proof.go
@@ -1,11 +1,15 @@
 package multi
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"fmt"
+	ics23 "github.com/confio/ics23/go"
 	verklestore "github.com/cosmos/cosmos-sdk/store/verkle"
-
+	"github.com/gballet/go-verkle"
 	"github.com/tendermint/tendermint/crypto/merkle"
 	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
+	"reflect"
 
 	types "github.com/cosmos/cosmos-sdk/store/v2"
 	"github.com/cosmos/cosmos-sdk/store/v2/smt"
@@ -39,8 +43,122 @@ func verkleProveKey(s *verklestore.Store, key []byte) (*tmcrypto.ProofOps, error
 		return nil, err
 	}
 	hkey := verklestore.Hash(key)
-	ret.Ops = append(ret.Ops, types.NewSmtCommitmentOp(hkey, keyProof).ProofOp())
+	ret.Ops = append(ret.Ops, types.NewVerkleCommitmentOp(hkey, keyProof).ProofOp())
 	return &ret, nil
+}
+
+// verifyIcs23VerkleProof verifies Verkle proofs for both existence and nonexistence and batch proofs.
+func verifyIcs23VerkleProofWithoutRoot(p *ics23.VerkleProof, kvs map[string][]byte) ([]byte, error) {
+	// TODO: should not rebuild verkle tree from KVs
+	tree := verkle.New()
+	if len(p.Value) != len(p.Key) {
+		return nil, fmt.Errorf("Temporary requirement: len of KV must match")
+	}
+	num := len(p.Value)
+	for i := 0; i < num; i++ {
+		err := tree.Insert(p.Key[i], p.Value[i], nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var keys [][]byte
+	for key, val := range kvs {
+		keys = append(keys, []byte(key))
+		val2, err := tree.Get([]byte(key), nil)
+		if err != nil {
+			return nil, err
+		}
+		if !reflect.DeepEqual(val, val2) {
+			return nil, fmt.Errorf("Value mismatch for key:" + key)
+		}
+	}
+
+	root := tree.ComputeCommitment().Bytes()
+	cfg, err := verkle.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+	proof, err := verkle.DeserializeProof(p.Proof)
+	if err != nil {
+		return nil, err
+	}
+	pe, _, _ := verkle.GetCommitmentsForMultiproof(tree, keys)
+	if !verkle.VerifyVerkleProof(proof, pe.Cis, pe.Zis, pe.Yis, cfg) {
+		return nil, fmt.Errorf("Failed to verify proof")
+	}
+
+	return root[:], nil
+}
+
+// keyPathValsToKeyVals converts map(key_path, value) to map(key, value)
+func keyPathValsToKeyVals(keypathVals map[string][]byte) (map[string][]byte, error) {
+	ret := make(map[string][]byte)
+	for kp, v := range keypathVals {
+		k, err := merkle.KeyPathToKeys(kp)
+		if err != nil {
+			return nil, err
+		}
+		if len(k) > 2 {
+			return nil, fmt.Errorf("unsupported key path")
+		}
+		ret[string(k[len(k)-1])] = v
+	}
+	return ret, nil
+}
+
+// VerifyVerkleProof verifies Verkle/Merkle proofs for both existence and nonexistence and batch proofs.
+func VerifyVerkleProof(proofOps *tmcrypto.ProofOps, rootCommitment []byte, keypathVals map[string][]byte) error {
+	if len(keypathVals) > 1 {
+		// TODO: add support for multiproofs
+		return fmt.Errorf("multiproofs not supported")
+	}
+	var root []byte
+	for _, p := range proofOps.GetOps() {
+		proofStr := p.GetData()
+		var ics23Proof ics23.CommitmentProof
+		err := ics23Proof.Unmarshal(proofStr)
+		if err != nil {
+			return err
+		}
+		if ics23Proof.GetVerkle() != nil {
+			keyVals, err := keyPathValsToKeyVals(keypathVals)
+			if err != nil {
+				return err
+			}
+			root, err = verifyIcs23VerkleProofWithoutRoot(ics23Proof.GetVerkle(), keyVals)
+			if err != nil {
+				return err
+			}
+		} else {
+			for kp, _ := range keypathVals {
+				k, err := merkle.KeyPathToKeys(kp)
+				if err != nil {
+					return err
+				}
+				if len(k) < 2 {
+					return fmt.Errorf("Invalid key path")
+				}
+			}
+			if root == nil {
+				return fmt.Errorf("Invalid ProofOps")
+			}
+			prt := DefaultProofRuntime()
+			operator, err := prt.Decode(p)
+			if err != nil {
+				return err
+			}
+			ret, err := operator.Run([][]byte{root})
+			if err != nil {
+				return err
+			}
+			root = ret[0]
+		}
+	}
+	if !bytes.Equal(rootCommitment, root) {
+		return fmt.Errorf("calculated root hash is invalid: expected %X but got %X", rootCommitment, root)
+	}
+	return nil
 }
 
 // GetProof returns ProofOps containing: a proof for the given key within this substore;
